e2e76c6e56f96bae18439e8824506239020261b1
Eric Rustrum <rustrumek@gmail.com>
2020-11-12 23:49:21 +0000

    Made some changes Got a friend to help with examples of natural language to parse with regular expressions.

--- chat.py
+++ chat.py
@@ -1,11 +1,13 @@
 import string, random
 
-auto = True
-training = False
+auto = False
+training = True
+debug = False
 
 async def respond(msg, data, ctx, client):
   global auto
   global training
+  global debug
 
   out = "<Unanticipated Input Error>"
   
@@ -14,22 +16,19 @@ async def respond(msg, data, ctx, client):
   punc = lower.translate(str.maketrans('', '', string.punctuation))
   split = punc.split(' ')
 
-  if auto: print(punc)
+  if auto and debug: print(punc)
   
   setin = open("set.in").read().split('\n')
-  if auto: print(setin)
+  if auto and debug: print(setin)
 
   setout = open("set.out").read().split('\n')
-  if auto: print(setout)
+  if auto and debug: print(setout)
 
   setrun = open("set.run").read().split('\n')
-  if auto: print(setrun)
+  if auto and debug: print(setrun)
 
-  setoutctx = open("setout.ctx").read().split('\n')
-  if auto: print(setoutctx)
-
-  setinctx = open("setin.ctx").read().split('\n')
-  if auto: print(setinctx)
+  topics = open("set.tpc").read().split('\n')
+  if auto and debug: print(topics)
 
   if (punc in setin) and auto:
     prob = []
@@ -47,6 +46,12 @@ async def respond(msg, data, ctx, client):
           spl[i] = client.get_user(int(spl[i])).mention
       out = ''.join(spl)
 
+    elif out == '.':
+      out = random.choice(topics)
+
+    elif out == '?':
+      out = 'Oh, I thought that made sense in my head, sorry!'
+
   else:
     if auto and training:
       open("zzz.imp", "+a").write(punc + '\n')
@@ -55,10 +60,11 @@ async def respond(msg, data, ctx, client):
       out = input('(' + msg.author.name + ')\n' + punc + "\n\n >")
       if training:
         if not((punc in setin) and (out in setout)):
-          open("set.in", "+a").write(punc + '\n')
-          open("set.out", "+a").write(out + '\n')
+          if not out == '#':
+            open("set.in", "+a").write(punc + '\n')
+            open("set.out", "+a").write(out + '\n')
 
 
   await msg.channel.send(out)
-  if auto: print(out)
+  if auto and debug: print(out)
   return punc, out
