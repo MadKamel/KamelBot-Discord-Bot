620123ddc652e3e57bffb315d0cd833fc54997ff
Eric Rustrum <rustrumek@gmail.com>
2020-11-18 22:46:56 +0000

    Integrated IRC features.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 # ( ͡° ͜ʖ ͡°)
-import discord, os, chat
+import discord, os, chat, irc, comms, threading
+os.system('clear')
+
+channel = "#mk-comms"
+server = "irc.freenode.net"
+nickname = "mk-comms-agent01"
+
+ircclient = irc.IRC()
+ircclient.connect(server, channel, nickname)
 
 
 intents = discord.Intents.all()
@@ -7,7 +15,6 @@ token = os.getenv('token')
 client = discord.Client(intents=intents)
 
 ctx = ['', '']
-
 
 
 @client.event
@@ -18,4 +25,26 @@ async def on_message(msg):
     
 
 
+def ircDaemonRoutine():
+  global ircclient
+  global channel
+  while True:
+    cmd, user = comms.parsecmd(ircclient.get_text())
+    if not cmd == None:
+      if cmd == 'ping':
+        ircclient.send(channel, 'pong')
+        print('ping from ' + user + ' ponged.')
+
+      elif cmd == 'pong':
+        print('pong from ' + user + ' recieved.')
+
+
+
+ircDaemon = threading.Thread(target=ircDaemonRoutine, daemon=True)
+ircDaemon.start()
+ircDaemon.run()
+ircDaemon.join()
+
+
+print('KamelBot going online.')
 client.run(token)
